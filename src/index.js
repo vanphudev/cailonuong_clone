1ad3e5bfe64d3851d3eed6c0ccc15c9841f9ccd2
vanphudev <nguyenvanphu.hufi@gmail.com>
2025-04-19 11:23:43 +0700

    init src

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const __root = ReactDOM.createRoot(document.getElementById("__root"));
+
+__root.render(
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>
+);
